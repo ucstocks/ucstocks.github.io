90c9d689c9a42718aabbe4b7f4c74d0d6290fb5b
Luke Trenaman <trenamanluke@gmail.com>
2022-02-26 13:23:02 -0500

    Add stock market API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Stocks from "./Stocks.js";
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Stocks/>
+
     </div>
   );
 }
