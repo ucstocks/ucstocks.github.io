3250862f47d2ff36628eeaf090f63b68044ad9dd
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2022-02-26 18:15:33 -0500

    added health bar

--- src/game/Components/header.js
+++ src/game/Components/header.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 function Header(props) {
+    const healthbarStyling = {
+        backgroundColor: "green",
+        width: String(props.health * 100) + "%",
+    }
     return (
         <div className="header-container">
             <div className="title-container">
                 <h1 className="title">{props.name}</h1>
             </div>
-            <h3 className="health">Health: {props.health}</h3>
+            <div className="health-container">
+                <h3 className="health">Health:</h3>
+                <div className="health-bar">
+                    <div style={healthbarStyling}>
+
+                    </div>
+                </div>
+            </div>
+            
             <h3 className="money">Money: ${props.money}</h3>
             <h3 className="assets">Assets: ${props.assets}</h3>
             <h3 className="date">Date: {props.date}</h3>
