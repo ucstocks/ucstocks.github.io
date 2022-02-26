511cd2f6e18bc4a2e74ad16beb4ea5aa587c0cc3
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2022-02-26 13:01:12 -0500

    Added components folder, created header

---
+++ src/Components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+class Header extends React.component {
+    render() {
+        <div id="header-container">
+            
+        </div>
+    }
+}
