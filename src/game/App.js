a968697bffb6c1f163072649daa212fc4641c4fc
Luke Trenaman <trenamanluke@gmail.com>
2022-02-26 16:39:34 -0500

    Add stock market class

--- src/game/App.js
+++ src/game/App.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header.js';
-import Stock from "./classes/stocks.js";
+import Market from "./classes/market.js";
 class App extends React.Component {
   constructor(){
     super();
-    this.state ={
+    this.market = new Market();
+    this.state = {
       "time":0,
       "name":"Ali",
       "stats":{
         "health":1,
         "happiness":1,
         "savings":1000,
-        "stocks":[new Stock("Pear","PEAR",50.23,2,0)]
+        "assets":0,
+        "stocks":{
+          "PEAR":2
+        }
       }
     };
+    this.state.stats.assets = this.market.netValue(this.state.stats.stocks);
   }
   tick(){
     return () => {
       let dat = this.state;
       dat.time += 1;
+      this.market.tick();
+      let val = this.market.netValue(dat.stats.stocks);
+      dat.stats.assets = val;
       this.setState(dat);
     }
   }
   render(){
-    console.log(this.state);
     return (
       <div className="App">
         
-        <Header />
+        <Header name={this.state.name}/>
         <div className="body">
           <div className="log-window">
           
@@ -37,6 +44,7 @@ class App extends React.Component {
             <h1>Start</h1>  
           </button>
           <p>{this.state.time}</p>
+          <p>Stocks assets: {this.state.stats.assets}</p>
         </div>
         
       </div>
--- src/game/classes/stocks.js
+++ src/game/classes/stocks.js
@@ -1,10 +1,14 @@
 class Stock{
-    constructor(name,ticker,price,amount,time){
+    constructor(name,ticker,price,description,marketCap,priceExpense){
         this.name = name;
         this.ticker = ticker;
         this.price = price;
-        this.amount = amount;
-        this.time = time;
+        this.description = description;
+        this.marketCap = marketCap;
+        this.priceExpense = this.priceExpense;
+    }
+    tick(){
+        this.price += Math.random() * this.price * 0.01;
     }
 }
 export default Stock;
